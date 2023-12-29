339834b15b9396cb2311f7af6a2a270c96ce7607
serii <seriiburduja@gmail.com>
2023-12-29 16:08:06 +0200

    feat: c-acf group remove and add works

--- c-acf.sh
+++ c-acf.sh
@@ -4,9 +4,12 @@ source /home/serii/Documents/bash-wp/functions/acf-functions.sh
 
 file_path=$(fzf)
 
-select action in "Add" "Remove" "Edit" "Exit"
+select action in "Show" "Add" "Remove" "Edit" "Exit"
 do
   case $action in
+    Show)
+      showFields $file_path
+      ;;
     Add)
       select type in "Group" "Field"
       do
@@ -15,17 +18,16 @@ do
             read -p "Enter the name of the group: " group_name
             newGroup "$group_name" "tab" $file_path
             newGroup "$group_name" "group" $file_path
-            exit 0
             ;;
           Field)
             read -p "Enter the name of the field: " field_name
             newGroup $field_name "field" $file_path
-            exit 0
             ;;
         esac
       done
       ;;
     Remove)
+      showFields $file_path
       removeGroup $file_path
       ;;
     Edit)
--- functions/acf-functions.sh
+++ functions/acf-functions.sh
@@ -1,4 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x
+
+function showFields(){
+  local file_path=$1
+  # read each item in the JSON array to an item in the Bash array
+  readarray -t my_array < <(jq --compact-output '.[0].fields[]' $file_path)
+
+  for item in "${my_array[@]}"; do
+    echo "------------------------------------"
+    local label=$(jq --raw-output '.label' <<< "$item")
+    local type=$(jq --raw-output '.type' <<< "$item")
+    echo "$type: $label"
+    echo "====================================="
+  done
+}
 
 function newGroup(){
   local id="$(openssl rand -base64 12)"
@@ -6,20 +20,35 @@ function newGroup(){
   local type=$2
   local file_path=$3
   local slug=$(echo $name | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+  if [ $type == "tab" ]; then
+    local result=$(cat $file_path | jq '.[0].fields[.[0].fields| length] += {
+    "key": "field_'${id}'",
+    "label": "'${name}'",
+    "type": "tab",
+    "placement": "top",
+    "endpoint": 0,
+    "date": "2010-01-07T19:55:99.999Z",
+    "xml": "xml_samplesheet_2017_01_07_run_09.xml",
+    "status": "OKKK",
+    "message": "metadata loaded into iRODS successfullyyyyy"
+  }')
+  echo $result > $file_path
+elif [ $type == "group" ]; then
   local result=$(cat $file_path | jq '.[0].fields[.[0].fields| length] += {
   "key": "field_'${id}'",
   "label": "'${name}'",
   "name": "'${slug}'",
-  "aria-label": "",
-  "type": "'${type}'",
+  "type": "group",
   "instructions": "",
   "required": 0,
   "conditional_logic": 0,
   "wrapper": {
   "width": "",
   "class": "",
-  "id": ""
+  "id": "",
 },
+"layout": "block",
+"sub_fields": [],
 "placement": "top",
 "endpoint": 0,
 "date": "2010-01-07T19:55:99.999Z",
@@ -28,6 +57,7 @@ function newGroup(){
 "message": "metadata loaded into iRODS successfullyyyyy"
 }')
 echo $result > $file_path
+  fi
 }
 
 export -f newGroup
@@ -41,21 +71,20 @@ readarray -t my_array < <(jq --compact-output '.[0].fields[]' $file_path)
 
 # iterate through the Bash array
 for item in "${my_array[@]}"; do
-  echo "========= start"
   local label=$(jq --raw-output '.label' <<< "$item")
   local type=$(jq --raw-output '.type' <<< "$item")
-  if [[ $type == "group" ]]; then
-    labels+=($label)
-  fi
+  labels+=($label)
 done
 
 select elem in "${labels[@]}"; do 
   [[ $elem ]] || continue
-  local key=$(jq -r '.[0].fields[] | select(.label == "'${elem}'") | .key' $file_path)
-  local result=$(cat $file_path | jq 'del(.[0].fields[] | select(.key == "'${key}'"))')
-  echo $result > $file_path
-  bat $file_path
-  exit 0
+  local key_tab=$(jq -r '.[0].fields[] | select(.label == "'${elem}'" and .type == "tab") | .key' $file_path)
+  local key_group=$(jq -r '.[0].fields[] | select(.label == "'${elem}'" and .type == "group") | .key' $file_path)
+  local result_tab=$(cat $file_path | jq 'del(.[0].fields[] | select(.key == "'${key_tab}'"))')
+  echo $result_tab > $file_path
+  local result_group=$(cat $file_path | jq 'del(.[0].fields[] | select(.key == "'${key_group}'"))')
+  echo $result_group > $file_path
+  showFields $file_path
 done
 key=$1
 file_path=$2
